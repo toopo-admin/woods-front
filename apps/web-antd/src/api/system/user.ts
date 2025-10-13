7d0417d1eab5a796db43fc8ac5dcc498af717ced
huangjiliang <785072557@qq.com>
2025-10-13 18:00:34 +0800

    用户管理

---
+++ apps/web-antd/src/api/system/user.ts
@@ -0,0 +1,75 @@
+import type { Recordable } from '@vben/types';
+
+import { requestClient } from '#/api/request';
+
+export namespace SystemUserApi {
+  export interface SystemUser {
+    [key: string]: any;
+    id: string;
+    username: string;
+    password: string;
+    nickName: string;
+    avatar: string;
+    phone: string;
+    email: string;
+    permissions: string[]; // 用户权限
+    enable: 1 | 2; // 1-启用，2-禁用
+  }
+}
+
+/**
+ * 获取用户列表数据
+ */
+async function getUserList(params: Recordable<any>) {
+  return requestClient.get<Array<SystemUserApi.SystemUser>>(
+    '/system/user/list',
+    { params },
+  );
+}
+
+/**
+ * 创建用户
+ * @param data 用户数据
+ */
+async function createUser(data: Omit<SystemUserApi.SystemUser, 'id'>) {
+  return requestClient.post('/system/user/create', data);
+}
+
+/**
+ * 更新用户
+ *
+ * @param id 用户 ID
+ * @param data 用户数据
+ */
+async function updateUser(
+  id: string,
+  data: Omit<SystemUserApi.SystemUser, 'id'>,
+) {
+  data.id = id;
+  return requestClient.post('/system/user/update', data);
+}
+
+/**
+ * 删除用户
+ * @param id 用户 ID
+ */
+async function deleteUser(id: string) {
+  return requestClient.post('/system/user/delete', { id });
+}
+
+/**
+ * 校验用户登录名是否存在
+ * @param id 表id
+ * @param roleCode 用户登录名
+ */
+async function isUserNameExist(
+  roleCode: string,
+  id?: SystemUserApi.SystemUser['id'],
+) {
+  return requestClient.get('/system/user/code-exist', {
+    params: { id, roleCode },
+    responseReturn: 'data',
+  });
+}
+
+export { createUser, deleteUser, getUserList, isUserNameExist, updateUser };
---
+++ apps/web-antd/src/views/system/user/data.ts
@@ -0,0 +1,138 @@
+import type { VbenFormSchema } from '#/adapter/form';
+import type { OnActionClickFn, VxeTableGridOptions } from '#/adapter/vxe-table';
+import { isUserNameExist, type SystemUserApi } from '#/api/system/user';
+
+import { ref } from 'vue';
+
+import { z } from '#/adapter/form';
+import { $t } from '#/locales';
+
+const formData = ref<SystemUserApi.SystemUser>();
+
+export function useFormSchema(): VbenFormSchema[] {
+  return [
+    {
+      component: 'Input',
+      fieldName: 'username',
+      label: $t('system.user.username'),
+      rules: z
+        .string()
+        .min(2, $t('ui.formRules.minLength', [$t('system.user.username'), 2]))
+        .max(30, $t('ui.formRules.maxLength', [$t('system.user.username'), 30]))
+        .refine(
+          async (value: string) => {
+            const res = await isUserNameExist(value, formData.value?.id);
+            return res.valid;
+          },
+          (value) => ({
+            message: $t('ui.formRules.alreadyExists', [
+              $t('system.user.username'),
+              value,
+            ]),
+          }),
+        ),
+    },
+    {
+      component: 'Input',
+      fieldName: 'roleName',
+      label: $t('system.role.roleName'),
+      rules: 'required',
+    },
+    {
+      component: 'RadioGroup',
+      componentProps: {
+        buttonStyle: 'solid',
+        options: [
+          { label: $t('common.enabled'), value: 1 },
+          { label: $t('common.disabled'), value: 2 },
+        ],
+        optionType: 'button',
+      },
+      defaultValue: 1,
+      fieldName: 'enable',
+      label: $t('system.role.status'),
+    },
+    {
+      component: 'Textarea',
+      fieldName: 'remark',
+      label: $t('system.role.remark'),
+    },
+    {
+      component: 'Input',
+      fieldName: 'permissions',
+      formItemClass: 'items-start',
+      label: $t('system.role.setPermissions'),
+      modelPropName: 'modelValue',
+    },
+  ];
+}
+
+export function useGridFormSchema(): VbenFormSchema[] {
+  return [
+    {
+      component: 'Input',
+      fieldName: 'roleCode',
+      componentProps: { allowClear: true },
+      label: $t('system.role.roleCode'),
+    },
+    {
+      component: 'Input',
+      fieldName: 'roleName',
+      componentProps: { allowClear: true },
+      label: $t('system.role.roleName'),
+    },
+  ];
+}
+
+export function useColumns<T = SystemUserApi.SystemUser>(
+  onActionClick: OnActionClickFn<T>,
+  onStatusChange?: (newStatus: any, row: T) => PromiseLike<boolean | undefined>,
+): VxeTableGridOptions['columns'] {
+  return [
+    {
+      field: 'roleCode',
+      title: $t('system.role.roleCode'),
+      width: 305,
+    },
+    {
+      field: 'roleName',
+      title: $t('system.role.roleName'),
+      width: 305,
+    },
+    {
+      cellRender: {
+        attrs: { beforeChange: onStatusChange },
+        name: onStatusChange ? 'CellSwitch' : 'CellTag',
+        props: { unCheckedValue: 2 },
+      },
+      field: 'enable',
+      title: $t('system.role.status'),
+      width: 200,
+    },
+    {
+      field: 'remark',
+      minWidth: 200,
+      title: $t('system.role.remark'),
+    },
+    {
+      field: 'createTime',
+      title: $t('system.role.createTime'),
+      width: 250,
+    },
+    {
+      align: 'center',
+      cellRender: {
+        attrs: {
+          nameField: 'name',
+          nameTitle: $t('system.role.name'),
+          onClick: onActionClick,
+        },
+        name: 'CellOperation',
+      },
+      field: 'operation',
+      fixed: 'right',
+      title: $t('system.role.operation'),
+      width: 200,
+    },
+  ];
+}
