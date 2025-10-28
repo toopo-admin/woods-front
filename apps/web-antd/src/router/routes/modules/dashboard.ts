91dceca3d60e7a1e44b446e8b928f350d500f853
huangjiliang <785072557@qq.com>
2025-10-28 17:39:25 +0800

    处理动态路由菜单

--- apps/web-antd/src/router/routes/modules/dashboard.ts
+++ apps/web-antd/src/router/routes/modules/dashboard.ts
@@ -4,34 +4,14 @@ import { $t } from '#/locales';
 
 const routes: RouteRecordRaw[] = [
   {
+    name: 'Analytics',
+    path: '/analytics',
+    component: () => import('#/views/dashboard/analytics/index.vue'),
     meta: {
-      icon: 'lucide:layout-dashboard',
-      order: -1,
+      affixTab: true,
+      icon: 'lucide:area-chart',
       title: $t('page.dashboard.title'),
     },
-    name: 'Dashboard',
-    path: '/dashboard',
-    children: [
-      {
-        name: 'Analytics',
-        path: '/analytics',
-        component: () => import('#/views/dashboard/analytics/index.vue'),
-        meta: {
-          affixTab: true,
-          icon: 'lucide:area-chart',
-          title: $t('page.dashboard.analytics'),
-        },
-      },
-      {
-        name: 'Workspace',
-        path: '/workspace',
-        component: () => import('#/views/dashboard/workspace/index.vue'),
-        meta: {
-          icon: 'carbon:workspace',
-          title: $t('page.dashboard.workspace'),
-        },
-      },
-    ],
   },
 ];
 
